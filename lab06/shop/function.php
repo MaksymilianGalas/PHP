5d213998328e409bbe1fa0c652806f5696588ac0
MaksymilianGalas <122981146+MaksymilianGalas@users.noreply.github.com>
2023-04-13 17:32:09 +0200

    Update function.php

--- lab06/shop/function.php
+++ lab06/shop/function.php
@@ -2,16 +2,17 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+
 function dodaj($id) {
     if (!isset($_SESSION['koszyk'])) {
         $_SESSION['koszyk'] = array();
     }
-
     if (isset($_SESSION['koszyk'][$id])) {
         $_SESSION['koszyk'][$id]++;
     } else {
         $_SESSION['koszyk'][$id] = 1;
     }
+    header("Location: sklep.php");
 }
 function usun($id) {
     if (isset($_SESSION['koszyk'][$id])) {
@@ -21,11 +22,12 @@ function usun($id) {
             unset($_SESSION['koszyk'][$id]);
         }
     }
+    header("Location: sklep.php");
 }
 function wyczysc() {
     unset($_SESSION['koszyk']);
-    session_destroy();
     dodaj(1);
     usun(1);
+    header("Location: sklep.php");
 }
 ?>
